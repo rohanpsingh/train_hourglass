0deb177692d9a39e89dd2454c9a8efe2d1852306
rohanpsingh <rohan565singh@gmail.com>
2018-12-13 11:49:47 +0900

    simplify concatenating images in batches

--- src/lua/main.lua
+++ src/lua/main.lua
@@ -3,19 +3,6 @@ paths.dofile('data.lua')    -- Set up data processing
 paths.dofile('model.lua')   -- Read in network model
 paths.dofile('train.lua')   -- Load up training/testing functions
 
---[=====[
-function loadPseudoData()
-    input_image = image.lena()
-    input_scale = 1.444
-    input_parts = torch.DoubleTensor({{100,1},{2,3},{4,5},{6,7},{8,9}})
-    input_center_x = 128
-    input_center_y = 128
-
-    annot.part = input_parts
-    annot.scale = input_scale
-    annot.center = torch.DoubleTensor({input_center_x, input_center_y})
-end
---]=====]
 
 function save_sample_image(inimage, filename)
     local rgb = inimage
@@ -27,10 +14,9 @@ end
 
 function loadInputData()
     os.execute('mkdir -p ' .. savePath_)
-    local timage = torch.cat(inImage_c3, inImage_c2, 3):cat(inImage_c1, 3):permute(3,1,2)
-    local input_image = unsqueeze:forward(timage:double())
-    input_image_batch = torch.cat(input_image_batch, input_image, 1)
-    save_sample_image(input_image[1], 'sample_image.jpg')
+    local input_image = torch.cat(inImage_c3, inImage_c2, 3):cat(inImage_c1, 3):permute(3,1,2):double()
+    input_image_batch = torch.cat(input_image_batch, input_image, 4)
+    save_sample_image(input_image, 'sample_image.jpg')
     annot.part = input_parts
     annot.scale = input_scale
     annot.center = torch.DoubleTensor({input_center_x, input_center_y})
@@ -40,7 +26,7 @@ function trainOnThis()
     -- Main training loop
     local input = torch.DoubleTensor()
     local label = {}
-    input, label = preprocessData(input_image_batch, annot.part, annot.center, annot.scale)
+    input, label = preprocessData(input_image_batch:permute(4,1,2,3), annot.part, annot.center, annot.scale)
     save_sample_image(input[1], 'processed.jpg')
     train(input, label)
     collectgarbage()
